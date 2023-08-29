2d0e294106adc726b09a1a689c4ea93e83eadbfe
Woongbin06 <kob05314@gmail.com>
2023-08-29 12:48:06 +0900

    FEAT :: Function

---
+++ src/main/kotlin/function/SumFunction.kt
@@ -0,0 +1,23 @@
+package function
+
+/**
+ * 최상위 함수는 어디에 있든 사용 가능.
+ */
+
+fun main() { // 최상위 함수
+    val result1 = sum(2, 3)
+    println("result1 = $result1")
+
+    val x: Int = 5;
+    val y: Int = 6;
+
+    println(max(x, y))
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun max(a: Int, b:Int): Int {
+    return if(a > b) a else b
+}
